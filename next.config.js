4a5db145a42e8929bd8d1fd52236f373a3bad822
kpietak <kpietak@agh.edu.pl>
2024-09-09 10:36:48 +0200

    initial commit
    Nextra Docs initial configuration added

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  latex: true,
+  defaultShowCopyCode: true,
+})
+
+module.exports = withNextra({
+  images: {
+    unoptimized: true
+  },
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/oop-course'
+  
+  
+})
